4933a1a5f334bbd57f04298cfb2cf23b7138124c
Susan <cho@Choui-MacBook-Pro-141.local>
2017-08-07 10:47:30 -0400

    Updating package.json and heroku

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const port = process.env.port || 3000;
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use(methodOverride('_method');
 
+
+mongoose.connect(mongoUri);
 app.listen(port, () =>{
-  console.log('listening...');
+  console.log('listening on port' + port);
 })
